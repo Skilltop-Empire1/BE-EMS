0757f2ff37a45582974ba7c7129f89dbdce11957
kingsley Arinze <kingsleyarinzeugwu@gmail.com>
2024-10-23 10:46:15 -0700

    update account

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const staffRoute = require("./routes/staffRoute");
 const patientRoute = require("./routes/patientRoute");
 const appointmentRoute = require("./routes/appointmentRoute");
 const reportRoute = require("./routes/reportRoute");
-const accountRoute = require("./routes/reportRoute");
+const accountRoute = require("./routes/accountRoutes");
 // const settingRoute = require("./routes/settingRoute");
 
 // Configure CORS
