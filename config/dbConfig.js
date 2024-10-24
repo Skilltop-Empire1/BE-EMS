b5b6c4f8854f2e20bbea658438b4444bcd2d0467
Augustine Ofor <100804858+Iupac120@users.noreply.github.com>
2024-10-24 20:06:32 +0100

    updated super admin path

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,44 +1,101 @@
+// // Import dependencies
+// const { Sequelize } = require("sequelize");
+// require("dotenv").config(); 
+
+
+// const CONFIG = {
+//   DB_NAME: process.env.DB_NAME,
+//   DB_USERNAME: process.env.DB_USERNAME,
+//   DB_PASSWORD: process.env.DB_PASSWORD,
+//   DB_DIALECT: process.env.DB_DIALECT || "postgres",
+//   DB_HOST: process.env.DB_HOST || "localhost",
+//   DB_PORT: parseInt(process.env.DB_PORT, 10) || 5432, 
+// };
+
+
+// const sequelize = new Sequelize(
+//   CONFIG.DB_NAME,
+//   CONFIG.DB_USERNAME,
+//   CONFIG.DB_PASSWORD,
+//   {
+//     host: CONFIG.DB_HOST,
+//     dialect: CONFIG.DB_DIALECT, 
+//     port: CONFIG.DB_PORT,
+//     logging: false, 
+//     dialectOptions: {
+//       ssl: {
+//         require: true, 
+//         rejectUnauthorized: false 
+//       },
+//       connectTimeout: 60000,
+//     },
+//   }
+// );
+
+// // Function to authenticate and sync the database
+// const initializeDatabase = async () => {
+//   try {
+//     await sequelize.authenticate();
+//     console.log("Connection to PostgreSQL database successful");
+
+//     await sequelize.sync({ alter: false }); 
+//     console.log("Database synchronized successfully");
+//   } catch (error) {
+//     console.error("Unable to connect to the PostgreSQL database:", error);
+//     throw error;
+//   }
+// };
+
+// // Initialize the database
+// initializeDatabase();
+
+// // Export the Sequelize instance
+// module.exports = sequelize;
+
+
 // Import dependencies
 const { Sequelize } = require("sequelize");
 require("dotenv").config(); 
 
 
+
+// Declaring database configuration parameters
 const CONFIG = {
   DB_NAME: process.env.DB_NAME,
   DB_USERNAME: process.env.DB_USERNAME,
   DB_PASSWORD: process.env.DB_PASSWORD,
   DB_DIALECT: process.env.DB_DIALECT || "postgres",
   DB_HOST: process.env.DB_HOST || "localhost",
-  DB_PORT: parseInt(process.env.DB_PORT, 10) || 5432, 
+  DB_PORT: parseInt(process.env.DB_PORT, 10) || 5432, // Default PostgreSQL port
 };
 
-
+// Create a new Sequelize instance
 const sequelize = new Sequelize(
   CONFIG.DB_NAME,
   CONFIG.DB_USERNAME,
   CONFIG.DB_PASSWORD,
   {
     host: CONFIG.DB_HOST,
-    dialect: CONFIG.DB_DIALECT, 
+    dialect: CONFIG.DB_DIALECT,
     port: CONFIG.DB_PORT,
-    logging: false, 
-    dialectOptions: {
-      ssl: {
-        require: true, 
-        rejectUnauthorized: false 
-      },
-      connectTimeout: 60000,
-    },
+    logging: false,
+    dialectOptions: CONFIG.DB_USE_SSL
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        }
+      : {}, // If not using SSL, leave it empty
   }
 );
 
-// Function to authenticate and sync the database
 const initializeDatabase = async () => {
   try {
     await sequelize.authenticate();
     console.log("Connection to PostgreSQL database successful");
 
-    await sequelize.sync({ alter: false }); 
+    await sequelize.sync( { alter: true });
     console.log("Database synchronized successfully");
   } catch (error) {
     console.error("Unable to connect to the PostgreSQL database:", error);
@@ -51,3 +108,17 @@ initializeDatabase();
 
 // Export the Sequelize instance
 module.exports = sequelize;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
