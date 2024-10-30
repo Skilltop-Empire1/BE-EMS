a459ddcf58dae32cab9889d1939e330ddb35471c
Augustine Ofor <100804858+Iupac120@users.noreply.github.com>
2024-10-30 14:43:46 +0100

    updated email

--- utils/mail.js
+++ utils/mail.js
@@ -8,28 +8,28 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = twilio(accountSid, authToken);
 
 const sendMail = async (email, subject, message) => {
-  // const transporter = await nodemailer.createTransport({
-  //   service: process.env.NODEMAIL_SERVICE,
-  //   auth: {
-  //     user: process.env.NODEMAIL_USER,
-  //     pass: process.env.NODEMAIL_PASS,
-  //   },
-  // });
-  
-  let transporter = nodemailer.createTransport({
-    host: "mail.skilltopims.com",
-    port: 587,
-    secure: false,
+  const transporter = await nodemailer.createTransport({
+    service: process.env.NODEMAIL_SERVICE,
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: process.env.NODEMAIL_USER,
+      pass: process.env.NODEMAIL_PASS,
     },
-    tls: {
-      rejectUnauthorized: false,
-    },
-    logger: true, 
-    debug: true,  
   });
+
+  // let transporter = nodemailer.createTransport({
+  //   host: "mail.skilltopims.com",
+  //   port: 587,
+  //   secure: false,
+  //   auth: {
+  //     user: process.env.EMAIL_USER,
+  //     pass: process.env.EMAIL_PASS,
+  //   },
+  //   tls: {
+  //     rejectUnauthorized: false,
+  //   },
+  //   logger: true, 
+  //   debug: true,  
+  // });
   
 
   const info = await transporter.sendMail({
